9044943143eb2dab9e0b03b55b41e2f1cdb90c29
Denys Huzovskyi <guderu@gmail.com>
2025-06-30 21:38:59 +0300

    feat: improve testutil uuid

--- internal/lib/testutil/uuid.go
+++ internal/lib/testutil/uuid.go
@@ -7,10 +7,11 @@ import (
 
 const uuidRegexp = `[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}`
 
-var uuidCompiledRegexp = regexp.MustCompile(uuidRegexp)
-
-var errNoUUIDFound = errors.New("no uuid found")
-var errEmptyText = errors.New("empty text")
+var (
+	uuidCompiledRegexp = regexp.MustCompile(uuidRegexp)
+	errNoUUIDFound     = errors.New("no uuid found")
+	errEmptyText       = errors.New("empty text")
+)
 
 func ExtractFirstUUIDFromText(text string) (string, error) {
 	if text == "" {
