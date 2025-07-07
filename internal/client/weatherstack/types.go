c0fa2b653286af67246bde0d01373bb4082d0999
Denys Huzovskyi <guderu@gmail.com>
2025-07-08 00:52:07 +0300

    fix: remove unused fields in weatherstack types

--- internal/client/weatherstack/types.go
+++ internal/client/weatherstack/types.go
@@ -2,10 +2,6 @@ package weatherstack
 
 type Location struct {
 	Name      string `json:"name"`
-	Country   string `json:"country"`
-	Region    string `json:"region"`
-	Lat       string `json:"lat"`
-	Lon       string `json:"lon"`
 	Timezone  string `json:"timezone_id"`
 	Localtime string `json:"localtime"`
 }
@@ -13,18 +9,8 @@ type Location struct {
 type Current struct {
 	ObservationTime     string   `json:"observation_time"`
 	Temperature         int      `json:"temperature"`
-	WeatherCode         int      `json:"weather_code"`
 	WeatherDescriptions []string `json:"weather_descriptions"`
-	WindSpeed           int      `json:"wind_speed"`
-	WindDegree          int      `json:"wind_degree"`
-	WindDir             string   `json:"wind_dir"`
-	Pressure            int      `json:"pressure"`
-	Precip              int      `json:"precip"`
 	Humidity            int      `json:"humidity"`
-	Cloudcover          int      `json:"cloudcover"`
-	Feelslike           int      `json:"feelslike"`
-	UVIndex             int      `json:"uv_index"`
-	Visibility          int      `json:"visibility"`
 }
 
 type CurrentWeather struct {
